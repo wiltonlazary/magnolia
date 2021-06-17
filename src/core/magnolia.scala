3045a5e63def831ca68ff83be50ec189c668578d
Kacper Korban <kacper.f.korban@gmail.com>
2021-06-18 00:18:48 +0200

    Revert params to overcomplicated version

--- src/core/magnolia.scala
+++ src/core/magnolia.scala
@@ -197,27 +197,64 @@ object MacroDerivation:
 
   def getParamsImpl[Typeclass[_]: Type, T: Type](using Quotes): Expr[List[CaseClass.Param[Typeclass, T]]] =
     import quotes.reflect.*
-    
+
+    def isRepeated[T](tpeRepr: TypeRepr): Boolean = tpeRepr match
+      case a: AnnotatedType =>
+        a.annotation.tpe match
+          case tr: TypeRef => tr.name == "Repeated"
+          case _           => false
+      case _ => false
+
+    def getTypeAnnotations(t: TypeRepr): List[Term] = t match
+      case AnnotatedType(inner, ann) => ann :: getTypeAnnotations(inner)
+      case _                         => Nil
+
+    def filterAnnotations(annotations: List[Term]): List[Term] =
+      annotations.filter { a =>
+        a.tpe.typeSymbol.maybeOwner.isNoSymbol ||
+          a.tpe.typeSymbol.owner.fullName != "scala.annotation.internal"
+      }
+
     val typeSymbol = TypeRepr.of[T].typeSymbol
+
+    val paramObj = '{CaseClass.Param}.asTerm
+    val paramConstrSymbol = TypeRepr.of[CaseClass.Param.type].termSymbol.declaredMethod("apply").head
+
+    val annotations = paramAnns[T].to(Map)
+    val typeAnnotations = paramTypeAnns[T].to(Map)
+
+    def termFromInlinedTypeApplyUnsafe(t: Term): Term = t match
+      case Inlined(_, _, TypeApply(term, _)) => term
 
     Expr.ofList {
       typeSymbol.caseFields.zipWithIndex.collect {
         case (paramSymbol: Symbol, idx: Int) =>
           val valdef: ValDef = paramSymbol.tree.asInstanceOf[ValDef]
           val paramTypeTree = valdef.tpt
-          paramTypeTree.tpe.asType match
-            case '[p] =>
-              '{
-                CaseClass.Param[Typeclass, T, p](
-                  "${Expr(paramSymbol.name)}" /*FIX ME*/,
-                  0 /*FIX ME*/,
-                  false /*FIX ME*/,
-                  CallByNeed(summonInline[Typeclass[p]]),
-                  CallByNeed(None),
-                  List.empty,
-                  List.empty
-                )
-              }
+          val paramTypeclassTree = Applied(TypeTree.of[Typeclass], List(paramTypeTree))
+          val summonInlineTerm = termFromInlinedTypeApplyUnsafe('{scala.compiletime.summonInline}.asTerm)
+          val summonInlineApp = TypeApply(summonInlineTerm, List(paramTypeclassTree))
+          val callByNeedTerm = '{CallByNeed}.asTerm
+          val callByNeedApply = TypeRepr.of[CallByNeed.type].termSymbol.declaredMethod("apply").head
+          val instance = Apply(TypeApply(Select(callByNeedTerm, callByNeedApply), List(paramTypeclassTree)), List(summonInlineApp))
+          Apply(
+            fun = TypeApply(
+              fun = Select(
+                qualifier = paramObj,
+                symbol = paramConstrSymbol
+              ),
+              args = List(TypeTree.of[Typeclass], TypeTree.of[T], paramTypeTree)
+            ),
+            args = List(
+              /*name =*/ Expr(paramSymbol.name).asTerm,
+              /*idx =*/ Expr(idx).asTerm,
+              /*repeated =*/ Expr(isRepeated(paramTypeTree.tpe)).asTerm,
+              /*cbn =*/ instance, //TODO FIX ME
+              /*defaultVal =*/ '{CallByNeed(None)}.asTerm,
+              /*annotations =*/ Expr.ofList(filterAnnotations(paramSymbol.annotations).toSeq.map(_.asExpr)).asTerm,
+              /*typeAnnotations =*/ Expr.ofList(filterAnnotations(getTypeAnnotations(paramTypeTree.tpe)).toSeq.map(_.asExpr)).asTerm
+            )
+          ).asExprOf[CaseClass.Param[Typeclass, T]]
       }
     }
 
